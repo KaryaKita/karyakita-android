e9d793a30f579d278a3bcf9c5238439fc83cda73
alifakbar111 <alifakbar111@gmail.com>
2018-09-24 20:09:28 +0700

    update-manifest

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/HomeCustomerActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/HomeCustomerActivity.java
@@ -1,17 +1,26 @@
 package com.karyakita.karyakita_android_new.activity.customer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.karyakita.karyakita_android_new.R;
@@ -41,15 +50,6 @@ public class HomeCustomerActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar_navigation);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab_application);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout_navigation);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -58,6 +58,15 @@ public class HomeCustomerActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view_application);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FloatingActionButton fab = findViewById(R.id.fab_application);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     ImageListener imageListener = new ImageListener() {
