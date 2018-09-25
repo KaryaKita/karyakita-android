e553514616ef669e1065b93b685f356d4a5d5d55
efriza <Efrizals@it.student.pens.ac.id>
2018-09-25 18:46:07 +0700

    update recycle view

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/DetailKaryaActivity.java
@@ -0,0 +1,28 @@
+package com.karyakita.karyakita_android_new.activity.customer;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.karyakita.karyakita_android_new.R;
+
+public class DetailKaryaActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_karya);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
