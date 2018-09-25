4a2e6f5ddfdad3cba8622cca58943e3eeda83158
Virdiawan IP <dvirdiawan@it.student.pens.ac.id>
2018-09-25 23:07:50 +0700

    new-movie-activity

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/model/MovieModel.java
@@ -0,0 +1,66 @@
+package com.karyakita.karyakita_android_new.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieModel {
+    @SerializedName("page")
+    @Expose
+    private int page;
+
+    @SerializedName("total_results")
+    @Expose
+    private int total_results;
+
+    @SerializedName("total_pages")
+    @Expose
+    private int total_pages;
+
+    @SerializedName("results")
+    @Expose
+    private List<ResultModel> result;
+
+    public MovieModel() {
+    }
+
+    public MovieModel(int page, int total_results, int total_pages, List<ResultModel> result) {
+        this.page = page;
+        this.total_results = total_results;
+        this.total_pages = total_pages;
+        this.result = result;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public List<ResultModel> getResult() {
+        return result;
+    }
+
+    public void setResult(List<ResultModel> result) {
+        this.result = result;
+    }
+}
