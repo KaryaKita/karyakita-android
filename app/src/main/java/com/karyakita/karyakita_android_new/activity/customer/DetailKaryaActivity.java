91c0c6858fc1a2f408238fee7f3be4ec5d87a1c8
efriza <Efrizals@it.student.pens.ac.id>
2018-09-29 00:07:26 +0700

    create-detail-karya

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/DetailKaryaActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/DetailKaryaActivity.java
@@ -1,24 +1,45 @@
 package com.karyakita.karyakita_android_new.activity.customer;
 
-import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.karyakita.karyakita_android_new.R;
+import com.karyakita.karyakita_android_new.model.MovieResponse;
+import com.karyakita.karyakita_android_new.view.ITestView;
 
-public class DetailKaryaActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+public class DetailKaryaActivity extends AppCompatActivity implements ITestView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detail_karya);
+        setContentView(R.layout.content_detail_karya);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+    public void showToast(String s) {
+
+    }
+
+    @Override
+    public void display(MovieResponse model) {
+
+    }
+
+    @Override
+    public void displayError(String s) {
+
     }
 
     @Override
