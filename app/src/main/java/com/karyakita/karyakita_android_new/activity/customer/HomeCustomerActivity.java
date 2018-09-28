d1f2dd662fc5fd7178848d7167ac1ebfe86b91ad
alifakbar111 <alifakbar111@gmail.com>
2018-09-28 17:46:35 +0700

    change-to-bind-view

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/HomeCustomerActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/HomeCustomerActivity.java
@@ -1,33 +1,23 @@
 package com.karyakita.karyakita_android_new.activity.customer;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ContentFrameLayout;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.karyakita.karyakita_android_new.R;
 import com.karyakita.karyakita_android_new.adapter.HomeCustomerAdapter;
-import com.karyakita.karyakita_android_new.fragment.AkunCustomerFragment;
-import com.karyakita.karyakita_android_new.fragment.DesainerFragment;
-import com.karyakita.karyakita_android_new.fragment.HomeFragment;
-import com.karyakita.karyakita_android_new.fragment.KategoriDesainFragment;
-import com.karyakita.karyakita_android_new.fragment.NotifikasiCustomerFragment;
-import com.karyakita.karyakita_android_new.fragment.PesananSayaFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,10 +30,7 @@ public class HomeCustomerActivity extends AppCompatActivity
     @BindView(R.id.drawer_layout_navigation) DrawerLayout drawerLayoutNavigation;
     @BindView(R.id.nav_view_application) NavigationView navViewApplication;
     @BindView(R.id.toolbar_navigation) Toolbar toolbarNavigation;
-
-    @SuppressLint("ResourceType")
-    @BindView(R.layout.home_customer_activity)
-    ContentFrameLayout homeCustomerActivity;
+    @BindView(R.id.fab_application) FloatingActionButton fabApplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,8 +66,8 @@ public class HomeCustomerActivity extends AppCompatActivity
     }
 
     public void setFloatingActionButton(){
-        FloatingActionButton fab = findViewById(R.id.fab_application);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        FloatingActionButton fab = findViewById(R.id.fab_application);
+        fabApplication.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -96,9 +83,9 @@ public class HomeCustomerActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout_navigation);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout_navigation);
+        if (drawerLayoutNavigation.isDrawerOpen(GravityCompat.START)) {
+            drawerLayoutNavigation.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -131,46 +118,25 @@ public class HomeCustomerActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        Fragment fragment = null;
-
         int id = item.getItemId();
-        switch (id){
-            case R.id.nav_beranda_customer:
-                fragment = new HomeFragment();
-                break;
-            case R.id.nav_kategori_customer:
-                fragment = new KategoriDesainFragment();
-                break;
-            case R.id.nav_desainer_customer:
-                fragment = new DesainerFragment();
-                break;
-            case R.id.nav_pesanan_saya_customer:
-                fragment = new PesananSayaFragment();
-                break;
-            case R.id.nav_notifikasi_customer:
-                fragment = new NotifikasiCustomerFragment();
-                break;
-            case R.id.nav_akun_customer:
-                fragment = new AkunCustomerFragment();
-                break;
-        }
 
-//        if (id == R.id.nav_beranda_customer) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_kategori_customer) {
-//
-//        } else if (id == R.id.nav_desainer_customer) {
-//
-//        } else if (id == R.id.nav_pesanan_saya_customer) {
-//
-//        } else if (id == R.id.nav_notifikasi_customer) {
-//
-//        } else if (id == R.id.nav_keluar) {
-//
-//        }
+        if (id == R.id.nav_beranda_customer) {
+
+        } else if (id == R.id.nav_kategori_desain_customer) {
+
+        } else if (id == R.id.nav_desainer_customer) {
+
+        } else if (id == R.id.nav_pesanan_saya_customer) {
+
+        } else if (id == R.id.nav_notifikasi_customer) {
+
+        } else if (id == R.id.nav_keluar) {
+
+        }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout_navigation);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
