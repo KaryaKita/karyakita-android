cf9b0731aa5de37a79593f536ce07b56825bf703
ffuroida <fatinfuroida@it.pens.ac.id>
2018-09-29 16:27:53 +0700

    update register

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/Register2Activity.java
@@ -0,0 +1,15 @@
+package com.karyakita.karyakita_android_new.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.karyakita.karyakita_android_new.R;
+
+public class Register2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
