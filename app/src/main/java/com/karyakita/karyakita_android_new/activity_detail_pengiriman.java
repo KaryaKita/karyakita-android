081490029d4503236b4165dcd9a5123d7210e9e7
ffuroida <fatinfuroida@it.pens.ac.id>
2018-10-01 11:37:29 +0700

    create detail order customer

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity_detail_pengiriman.java
@@ -0,0 +1,13 @@
+package com.karyakita.karyakita_android_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_detail_pengiriman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_order);
+    }
+}
