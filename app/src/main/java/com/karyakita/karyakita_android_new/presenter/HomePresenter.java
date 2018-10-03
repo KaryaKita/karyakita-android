f7bedfbbdcf669451ccb2c283559dfd4fe63072e
alifakbar111 <alifakbar111@gmail.com>
2018-10-03 19:15:01 +0700

    create-new-home-presenter

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/presenter/HomePresenter.java
@@ -0,0 +1,20 @@
+package com.karyakita.karyakita_android_new.presenter;
+
+import com.karyakita.karyakita_android_new.base_class_interface.IMainPresenter;
+
+public class HomePresenter implements IMainPresenter {
+    @Override
+    public void get() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void insert() {
+
+    }
+}
