7874040a7f805985a5773c6b6106b0803cfcf33f
Virdiawan IP <dvirdiawan@it.student.pens.ac.id>
2018-10-03 15:28:57 +0700

    add_customer_pesan_selesai

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/CustomerPesanSelesaiActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/CustomerPesanSelesaiActivity.java
@@ -3,11 +3,13 @@ package com.karyakita.karyakita_android_new.activity.customer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.karyakita.karyakita_android_new.R;
+
 public class CustomerPesanSelesaiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pesan_selesai_activity);
+        setContentView(R.layout.customer_pesan_selesai_activity);
     }
 }
