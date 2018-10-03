496faa16dc687d55bb614791795670a3ed1884b3
Virdiawan IP <dvirdiawan@it.student.pens.ac.id>
2018-10-03 15:13:43 +0700

    add_konfirmasi_bayar

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/CustomerKonfirmasiBayarActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/customer/CustomerKonfirmasiBayarActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.karyakita.karyakita_android_new.R;
 
-public class KonfirmasiBayarActivity extends AppCompatActivity {
+public class CustomerKonfirmasiBayarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.konfirmasi_bayar_activity);
+        setContentView(R.layout.customer_konfirmasi_bayar_activity);
     }
 }
