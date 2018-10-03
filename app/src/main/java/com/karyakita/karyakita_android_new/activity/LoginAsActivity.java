8018fd0aead0c24e8abcd8925b900fad4d4aef36
alifakbar111 <alifakbar111@gmail.com>
2018-10-03 15:42:57 +0700

    create-activity

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/LoginAsActivity.java
@@ -0,0 +1,21 @@
+package com.karyakita.karyakita_android_new.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.karyakita.karyakita_android_new.R;
+
+import butterknife.ButterKnife;
+
+public class LoginAsActivity extends AppCompatActivity {
+
+//    @BindView(R.id.)
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_as);
+        ButterKnife.bind(this);
+    }
+}
