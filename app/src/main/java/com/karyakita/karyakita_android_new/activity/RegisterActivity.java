34ba303f90d75f236e7678945987391e25c5ca21
ffuroida <fatinfuroida@it.pens.ac.id>
2018-10-03 15:50:46 +0700

    register activity

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/RegisterActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/RegisterActivity.java
@@ -4,12 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.karyakita.karyakita_android_new.R;
+import com.karyakita.karyakita_android_new.model.MovieResponse;
+import com.karyakita.karyakita_android_new.view.ITestView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements ITestView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void showToast(String s) {
+
+    }
+
+    @Override
+    public void display(MovieResponse model) {
+
+    }
+
+    @Override
+    public void displayError(String s) {
+
     }
 }
