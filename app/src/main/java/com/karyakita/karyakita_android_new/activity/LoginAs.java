e88edf6fb8d61bed0d3ca97432f76fd19403a9d1
alifakbar <mbenypangestu@it.student.pens.ac.id>
2018-10-03 16:27:53 +0700

    Update conflix

--- app/src/main/java/com/karyakita/karyakita_android_new/activity/LoginAs.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/activity/LoginAs.java
@@ -16,7 +16,7 @@ public class LoginAs extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_as);
+        setContentView(R.layout.activity_login_as);
         ButterKnife.bind(this);
     }
 }
