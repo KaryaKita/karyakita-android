aab05bea9fb242a75a9f097e522c2caeaa084247
alifakbar111 <alifakbar111@gmail.com>
2018-10-06 16:16:51 +0700

    edit-OnNavigationItemSelected

--- app/src/main/java/com/karyakita/karyakita_android_new/customer/home/HomeCustomerActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/customer/home/HomeCustomerActivity.java
@@ -1,11 +1,14 @@
 package com.karyakita.karyakita_android_new.customer.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -17,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.karyakita.karyakita_android_new.R;
+import com.karyakita.karyakita_android_new.customer.karya.ListKaryaActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -125,12 +129,13 @@ public class HomeCustomerActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Fragment fragment = null;
         int id = item.getItemId();
 
         if (id == R.id.nav_beranda_customer) {
-
+            fragment = new HomeFragment();
         } else if (id == R.id.nav_kategori_desain_customer) {
-
+            startActivity(new Intent(HomeCustomerActivity.this, ListKaryaActivity.class));
         } else if (id == R.id.nav_desainer_customer) {
 
         } else if (id == R.id.nav_pesanan_saya_customer) {
@@ -139,6 +144,11 @@ public class HomeCustomerActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_keluar) {
 
+        }
+        if (fragment != null){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.view_pager_home_customer, fragment);
+            fragmentTransaction.commit();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout_navigation);
