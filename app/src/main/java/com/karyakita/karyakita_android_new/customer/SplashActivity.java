db6966077e0002f16d68fbb7f65a2307c51c2951
ffuroida <fatinfuroida@it.pens.ac.id>
2018-10-06 15:07:26 +0700

    update list karya

--- app/src/main/java/com/karyakita/karyakita_android_new/customer/SplashActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/customer/SplashActivity.java
@@ -5,13 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.karyakita.karyakita_android_new.customer.home.HomeCustomerActivity;
+import com.karyakita.karyakita_android_new.login.LoginAsActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, HomeCustomerActivity.class));
+        startActivity(new Intent(SplashActivity.this, LoginAsActivity.class));
         finish();
     }
 }
