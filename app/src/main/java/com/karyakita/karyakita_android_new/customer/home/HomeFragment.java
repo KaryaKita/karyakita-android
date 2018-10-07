32a71dcfe6c4477be5475e89aa4e8a69ed9471ab
alifakbar111 <alifakbar111@gmail.com>
2018-10-08 01:09:20 +0700

    reformat-code

--- app/src/main/java/com/karyakita/karyakita_android_new/customer/home/HomeFragment.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/customer/home/HomeFragment.java
@@ -6,15 +6,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.karyakita.karyakita_android_new.R;
@@ -23,11 +20,8 @@ import com.karyakita.karyakita_android_new.customer.karya.DetailKaryaActivity;
 import com.karyakita.karyakita_android_new.customer.karya.KategoriKaryaAdapter;
 import com.karyakita.karyakita_android_new.customer.karya.KategoriKaryaModel;
 import com.karyakita.karyakita_android_new.customer.karya.KategoriKaryaResultModel;
-import com.karyakita.karyakita_android_new.customer.karya.ListKaryaActivity;
 
 import java.util.List;
-
-import butterknife.BindView;
 
 import static android.support.constraint.Constraints.TAG;
 
@@ -46,14 +40,13 @@ public class HomeFragment extends Fragment implements IHomeView {
 //    RecyclerView rv_image_home;
 
 
-
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_customer, container, false);
 
-        im_list_image1= view.findViewById(R.id.list_image1);
-        im_list_image2= view.findViewById(R.id.list_image2);
-        im_list_image3= view.findViewById(R.id.list_image3);
-        im_list_image4= view.findViewById(R.id.list_image4);
+        im_list_image1 = view.findViewById(R.id.list_image1);
+        im_list_image2 = view.findViewById(R.id.list_image2);
+        im_list_image3 = view.findViewById(R.id.list_image3);
+        im_list_image4 = view.findViewById(R.id.list_image4);
 
         im_list_image1.setOnClickListener(new View.OnClickListener() {
             @Override
