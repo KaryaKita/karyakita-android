297ee4e423ff0b362027892b6f4fb79504b47f70
efriza <rezaprillia@it.student.pens.ac.id>
2018-10-15 18:54:45 +0700

    layout UI

--- app/src/main/java/com/karyakita/karyakita_android_new/desainer/list_desainer/ListDesainerActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/desainer/list_desainer/ListDesainerActivity.java
@@ -2,17 +2,44 @@ package com.karyakita.karyakita_android_new.desainer.list_desainer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
+import com.karyakita.karyakita_android_new.R;
 import com.karyakita.karyakita_android_new.customer.karya.ListKaryaModel;
+
+import butterknife.BindView;
 
 public class ListDesainerActivity extends AppCompatActivity implements IListDesainerView{
     ListDesainerPresenter listDesainerPresenter = null;
     ListKaryaModel listKaryaModel = null;
+    private String TAG = "ListDesainerActivity";
 
-    @Override
-    protected void onCreateView(){
+    @BindView(R.id.rv_list_desainer)
+    RecyclerView rv_list_desainer;
+
+    protected void onCreateView(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
 
     }
 
+    @Override
+    public void showToast(String s) {
 
+    }
+
+    @Override
+    public void display(ListDesainerResultModel listDesainerResultModel) {
+
+    }
+
+    @Override
+    public void displayError(String s) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
