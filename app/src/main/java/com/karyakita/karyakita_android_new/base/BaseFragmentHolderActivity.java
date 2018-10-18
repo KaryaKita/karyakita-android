762d751a0aab6b12ef1d19dedad85acd96fbcb58
alifakbar <alifakbar111@it.student.pens.ac.id>
2018-10-18 17:04:54 +0700

    update toolbar

--- app/src/main/java/com/karyakita/karyakita_android_new/base/BaseFragmentHolderActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/base/BaseFragmentHolderActivity.java
@@ -1,6 +1,21 @@
 package com.karyakita.karyakita_android_new.base;
 
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 public class BaseFragmentHolderActivity extends BaseActivity {
+    protected TextView tvToolbarTitle;
+    protected FrameLayout flFragmentContainer;
+    protected ImageButton btOptionMenu;
+    protected ImageView ivIcon;
+    protected ImageButton btBack;
+    protected View vMenuBarShadow;
+    protected RelativeLayout rlActivityFragmentHolder;
+
     @Override
     protected void initView() {
 
@@ -8,11 +23,6 @@ public class BaseFragmentHolderActivity extends BaseActivity {
 
     @Override
     protected void initFragment() {
-
-    }
-
-    @Override
-    public void setTitle(String title) {
 
     }
 }
