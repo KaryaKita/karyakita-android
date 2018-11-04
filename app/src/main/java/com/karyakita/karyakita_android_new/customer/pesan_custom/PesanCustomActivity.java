c800732df65db55f0492384fcffaa4517bc53e08
ffuroida <fatinfuroida@it.pens.ac.id>
2018-11-04 18:57:41 +0700

    test update

--- app/src/main/java/com/karyakita/karyakita_android_new/customer/pesan_custom/PesanCustomActivity.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/customer/pesan_custom/PesanCustomActivity.java
@@ -1,20 +1,29 @@
 package com.karyakita.karyakita_android_new.customer.pesan_custom;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import com.karyakita.karyakita_android_new.R;
+import com.mlsdev.rximagepicker.RxImagePicker;
+import com.mlsdev.rximagepicker.Sources;
+
+import butterknife.BindView;
 
 public class PesanCustomActivity extends AppCompatActivity {
 
     Spinner spinner;
+    @BindView(R.id.iv_img_custom)
+    ImageView img_custom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesan_custom);
 
+//        RxImagePicker.with(this).requestImage(Sources.GALLERY)
 
     }
 }
