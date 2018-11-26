90e308f55aba8a7399b86ea50c37e5820434b863
Virdiawan IP <dvirdiawan@it.student.pens.ac.id>
2018-11-26 07:25:36 +0700

    add_desainer_edit_bio

---
+++ app/src/main/java/com/karyakita/karyakita_android_new/desainer/edit_bio/DesainerEditBioActivity.java
@@ -0,0 +1,15 @@
+package com.karyakita.karyakita_android_new.desainer.edit_bio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.karyakita.karyakita_android_new.R;
+
+public class DesainerEditBioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desainer_edit_bio);
+    }
+}
---
+++ app/src/main/java/com/karyakita/karyakita_android_new/desainer/edit_bio/DesainerEditBioResultModel.java
@@ -0,0 +1,4 @@
+package com.karyakita.karyakita_android_new.desainer.edit_bio;
+
+public class DesainerEditBioResultModel {
+}
