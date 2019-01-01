e76a2f80ef83b90d43e4cb9421558be055d41f91
alifakbar111 <alifakbar111@gmail.com>
2019-01-02 00:06:41 +0700

    remove crash error merge

--- app/src/main/java/com/karyakita/karyakita_android_new/login/LoginHelper.java
+++ app/src/main/java/com/karyakita/karyakita_android_new/login/LoginHelper.java
@@ -2,16 +2,7 @@ package com.karyakita.karyakita_android_new.login;
 
 import android.util.Log;
 
-<<<<<<< HEAD
-import com.karyakita.karyakita_android_new.data.local.realm.RealmHelper;
-import com.karyakita.karyakita_android_new.data.local.realm.model.PesanLangsungModel;
-
-import java.util.List;
-
-=======
->>>>>>> 01b45f7bf471566ba9ed6f0dc1935b1b3f676c39
 import io.realm.Realm;
-import io.realm.RealmResults;
 
 public class LoginHelper {
     Realm realm;
@@ -46,7 +37,7 @@ public class LoginHelper {
     }
 
 
-    public SessionModel getUser(){
+    public SessionModel getUser() {
         SessionModel results = realm.where(SessionModel.class).findFirst();
         return results;
     }
